26761edeee7891f07426fc8c9cb248a398931caa
revalidate <reva.bhatt@gmail.com>
2015-11-27 08:16:54 -0800

    two players within one div that can move both left & right

--- game-1/main.js
+++ game-1/main.js
@@ -1,14 +1,28 @@
 console.log("Sanity Check: JS is working!");
 
-var playerRight = 0;
-var playerLeft = 0;
+var playerOneLeft = 0;
+var playerOneRight = 0;
+var playerOneUp = 0;
+var playerOneDown = 0;
+var playerTwoLeft = 0;
+var playerTwoRight = 0;
+var playerTwoUp = 0;
+var playerTwpDown = 0;
 
 $(document).ready(function() {
 	$(document).keydown(function (e) {
-        if(e.keyCode == 39) {
- 			playerRight += 5;
-        } else if (e.keyCode==37) {
-        	playerLeft +=5;
+        if(e.keyCode == 88) {
+ 			playerOneLeft += 5;
+ 			$("#player1").css({left:playerOneLeft});
+ 		} else if (e.keyCode ==90) {
+ 			playerOneRight -= 5;
+ 			$("#player1").css({left:playerOneRight});
+ 		} else if (e.keyCode == 39) {
+        	playerTwoLeft += 5;
+        	$("#player2").css({left:playerTwoLeft});
+        } else if (e.keyCode == 37) {
+        	playerTwoRight -= 5;
+        	$("#player2").css({left:playerTwoRight});
         }
 	});
 });
