5fca213cd2e9611401b7a2ef2ca8ca3b8247f2b1
revalidate <reva.bhatt@gmail.com>
2015-11-28 12:43:24 -0800

    added functioning reset button, winning div box

--- game-1/main.js
+++ game-1/main.js
@@ -7,22 +7,44 @@ var playerOneDown = 0;
 var playerTwoLeft = 0;
 var playerTwoRight = 0;
 var playerTwoUp = 0;
-var playerTwpDown = 0;
+var playerTwoDown = 0;
 
 $(document).ready(function() {
 	$(document).keydown(function (e) {
-        if(e.keyCode == 88) {
+		var guy1Left = $("#player1").position().left;
+		var guy1Top = $("#player1").position().top;
+		var guy2Left = $("#player2").position().left;
+		var guy2Top = $("#player2").position().top;
+        if (e.keyCode ===83) {
  			playerOneLeft += 5;
  			$("#player1").css({left:playerOneLeft});
- 		} else if (e.keyCode ==90) {
+ 		} else if (e.keyCode ===65) {
  			playerOneRight -= 5;
  			$("#player1").css({left:playerOneRight});
- 		} else if (e.keyCode == 39) {
+ 		} else if (e.keyCode ===90) {
+ 			playerOneUp += 5;
+ 			$("#player1").css({top:playerOneUp});
+ 		} else if (e.keyCode ===87) {
+ 			playerOneDown -= 5;
+ 			$("#player1").css({top:playerOneDown});
+ 		} else if (e.keyCode === 39) {
         	playerTwoLeft += 5;
         	$("#player2").css({left:playerTwoLeft});
-        } else if (e.keyCode == 37) {
+        } else if (e.keyCode === 37) {
         	playerTwoRight -= 5;
         	$("#player2").css({left:playerTwoRight});
+        } else if (e.keyCode === 40) {
+        	playerTwoUp += 5;
+        	$("#player2").css({top:playerTwoUp});
+        } else if (e.keyCode === 38) {
+        	playerTwoDown -= 5;
+        	$("#player2").css({top:playerTwoDown});
         }
+	});
+	$("button").click(function reset() {
+		$("#player1").css('left', 0);
+		$("#player1").css('top', 0);
+		$("#player2").css('left', 0);
+		$("#player2").css('top', 400);
 	});
 });
