85dcdb03ddf8ade84a39b46eacb4f3badcb69e3d
revalidate <reva.bhatt@gmail.com>
2015-11-29 17:19:31 -0800

    game responds to winner via html, moving players don't jump position anymore, once one player wins, other player can't move

--- game-2/main.js
+++ game-2/main.js
@@ -1,59 +1,73 @@
 console.log("Sanity Check: JS is working!");
 
-var playerOneLeft = 0;
-var playerOneRight = 0;
-var playerOneUp = 0;
-var playerOneDown = 0;
-var playerTwoLeft = 0;
-var playerTwoRight = 0;
-var playerTwoUp = 0;
-var playerTwoDown = 0;
+var player1LeftRight = 0;
+var player1UpDown = 0;
+var player2LeftRight = 0;
+var player2UpDown = 400;
 
 $(document).ready(function() {
-	$(document).keydown(function (e) {
-		var guy1Left = $("#player1").position().left;
-		var guy1Top = $("#player1").position().top;
-		var guy2Left = $("#player2").position().left;
-		var guy2Top = $("#player2").position().top;
-        if (e.keyCode ===83) {
- 			playerOneLeft += 5;
- 			$("#player1").css({left:playerOneLeft});
- 		} else if (e.keyCode ===65) {
- 			playerOneRight -= 5;
- 			$("#player1").css({left:playerOneRight});
- 		} else if (e.keyCode ===90) {
- 			playerOneUp += 5;
- 			$("#player1").css({top:playerOneUp});
- 		} else if (e.keyCode ===87) {
- 			playerOneDown -= 5;
- 			$("#player1").css({top:playerOneDown});
- 		} else if (e.keyCode === 39) {
-        	playerTwoLeft += 5;
-        	$("#player2").css({left:playerTwoLeft});
-        } else if (e.keyCode === 37) {
-        	playerTwoRight -= 5;
-        	$("#player2").css({left:playerTwoRight});
-        } else if (e.keyCode === 40) {
-        	playerTwoUp += 5;
-        	$("#player2").css({top:playerTwoUp});
-        } else if (e.keyCode === 38) {
-        	playerTwoDown -= 5;
-        	$("#player2").css({top:playerTwoDown});
-        }
- 		checkWinner();
-	});
+	function startGame() {
+		$(document).keydown(function (e) {
+			var guy1Left = $("#player1").position().left;
+			var guy1Top = $("#player1").position().top;
+			var guy2Left = $("#player2").position().left;
+			var guy2Top = $("#player2").position().top;
+	        if (e.keyCode ===83) {
+	 			player1LeftRight += 20;
+	 			$("#player1").css({left:player1LeftRight});
+	 		} else if (e.keyCode ===65) {
+	 			player1LeftRight -= 20;
+	 			$("#player1").css({left:player1LeftRight});
+	 		} else if (e.keyCode ===90) {
+	 			player1UpDown += 20;
+	 			$("#player1").css({top:player1UpDown});
+	 		} else if (e.keyCode ===87) {
+	 			player1UpDown -= 20;
+	 			$("#player1").css({top:player1UpDown});
+	 		} else if (e.keyCode === 39) {
+	        	player2LeftRight += 20;
+	        	$("#player2").css({left:player2LeftRight});
+	        } else if (e.keyCode === 37) {
+	        	player2LeftRight -= 20;
+	        	$("#player2").css({left:player2LeftRight});
+	        } else if (e.keyCode === 40) {
+	        	player2UpDown += 20;
+	        	$("#player2").css({top:player2UpDown});
+	        } else if (e.keyCode === 38) {
+	        	player2UpDown -= 20;
+	        	$("#player2").css({top:player2UpDown});
+	        }
+	 		checkWinner();
+		});
+	}
+
+//Reset Button
 	$("button").click(function reset() {
 		$("#player1").css('left', 0);
 		$("#player1").css('top', 0);
 		$("#player2").css('left', 0);
 		$("#player2").css('top', 400);
+		player1LeftRight = 0;
+		player1UpDown = 0;
+		player2LeftRight = 0;
+		player2UpDown = 400;
+		$("#winnerName").html("Ready, Set, Go!");
+		$("#winnerConditions").html("Who can get inside the box first?");
+		startGame();
 	});
+
+	startGame();
 });
 
+//Checks Winner
 function checkWinner() {
 	if ($('#player1').css('left')== "1000px" && $('#player1').css('top')=="200px") {
-    	alert("Lakshmi wins");
+    	$("#winnerConditions").html("WINNER: ");
+    	$("#winnerName").html("Lakshmi");
+    	$(document).off("keydown");
 	} else if ($('#player2').css('left')== "1000px" && $('#player2').css('top')=="200px") {
-    	alert("Ganesha wins");
+     	$("#winnerConditions").html("WINNER: ");
+     	$("#winnerName").html("Ganesha");
+     	$(document).off("keydown");
 	}
 }
